d8eb0db825cbd209883788aff77000ca7b7a78bc
ahackkk <aktandoolatkeldiev@gmail.com>
2024-09-30 23:21:37 +0600

    +OneClass

--- src/Main.java
+++ src/Main.java
@@ -2,44 +2,40 @@ import java.util.Scanner;
 
 public class Main{
     public static void main(String[] args) {
-        print("If you are ready to play, type 'yes'");
         Scanner system = new Scanner(System.in);
+
+        Methods.print("If you are ready to play, type 'yes'");
         String answer = system.nextLine();
+
         if(!answer.equals("yes")){
-            exitProgram();
+            Methods.exitProgram();
         }
         else{
-            print("Question 1\n We are using only 10% of our brain\n Type 'true' or 'false'");
+            Methods.print("Question 1\n We are using only 10% of our brain\n Type 'true' or 'false'");
             String answer2 = system.nextLine();
+
             if(!answer2.equals("false")){
-                exitProgram();
+                Methods.exitProgram();
             }
             else{
-                print("Question 2\n Reading in a lite darkness is not affecting your eyesight\n Type 'true' or 'false'");
+                Methods.print("Question 2\n Reading in a lite darkness is not affecting your eyesight\n Type 'true' or 'false'");
                 String answer3 = system.nextLine();
+
                 if(!answer3.equals("true")){
-                    exitProgram();
+                    Methods.exitProgram();
                 }
                 else{
-                    print("Question 3\n Is Emilbek agay crush?\n Type 'true' or 'false'");
+                    Methods.print("Question 3\n Is Emilbek agay crush?\n Type 'true' or 'false'");
                     String answer4 = system.nextLine();
+
                     if(answer4.equals("true")){
-                        print("gay");
+                        Methods.print("gay");
                     }
                     else{
-                        print("shakal");
+                        Methods.print("shakal");
                     }
                 }
             }
         }
-    }
-
-    public static void print(String message){
-        System.out.println(message);
-    }
-
-    public static void exitProgram(){
-        System.exit(1);
-        System.out.println("Invalid syntaxes");
     }
 }
---
+++ src/Methods.java
@@ -0,0 +1,10 @@
+public class Methods {
+    public static void print(String message){
+        System.out.println(message);
+    }
+
+    public static void exitProgram(){
+        System.exit(1);
+        System.out.println("Invalid syntaxes");
+    }
+}
