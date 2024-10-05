0348b5dbc464326f560c1d09b5bfc95b3fa2c213
ahackkk <aktandoolatkeldiev@gmail.com>
2024-10-05 23:56:27 +0600

    AddingWhile

--- src/Main.java
+++ src/Main.java
@@ -2,48 +2,60 @@ import java.util.Scanner;
 
 public class Main{
     public static void main(String[] args) {
-        Scanner system = new Scanner(System.in);
 
+        Scanner system = new Scanner(System.in);
         Methods.print("If you are ready to play, type 'yes'");
         String answer = system.nextLine();
-        int score = 0;
 
-        if(!answer.equals("yes")){
-            Methods.exitProgram();
-            Methods.print("Incorrect");
+        while(!answer.equals("yes")){
+            Methods.print("Get ready");
+            String tryanswer = system.nextLine();
         }
-        else{
-            Methods.print("Question 1\n We are using only 10% of our brain\n Type 'true' or 'false'");
-            String answer2 = system.nextLine();
-            score++;
 
-            if(!answer2.equals("false")){
-                Methods.exitProgram();
-                Methods.print("Incorrect");
-            }
-            else{
-                Methods.print("Question 2\n Reading in a lite darkness is not affecting your eyesight\n Type 'true' or 'false'");
-                String answer3 = system.nextLine();
-                score++;
+        Methods.print("Question 1\n We are using only 10% of our brain\n Type 'true' or 'false'");
+        String answer2 = system.nextLine();
 
-                if(!answer3.equals("true")){
-                    Methods.exitProgram();
-                    Methods.print("Incorrect");
-                }
-                else{
-                    Methods.print("Question 3\n Is Emilbek agay crush?\n Type 'true' or 'false'");
-                    String answer4 = system.nextLine();
-                    score++;
-
-                    if(answer4.equals("true")){
-                        Methods.print("gay");
-                        Methods.print(String.valueOf(score));
-                    }
-                    else{
-                        Methods.print("shakal");
-                    }
-                }
-            }
+        while (!answer2.equals("false")){
+            Methods.print("You are dumbass");
         }
+//        int score = 0;
+//
+//        if(!answer.equals("yes")){
+//            Methods.exitProgram();
+//            Methods.print("Incorrect");
+//        }
+//        else{
+//            Methods.print("Question 1\n We are using only 10% of our brain\n Type 'true' or 'false'");
+//            String answer2 = system.nextLine();
+//            score++;
+//
+//            if(!answer2.equals("false")){
+//                Methods.exitProgram();
+//                Methods.print("Incorrect");
+//            }
+//            else{
+//                Methods.print("Question 2\n Reading in a lite darkness is not affecting your eyesight\n Type 'true' or 'false'");
+//                String answer3 = system.nextLine();
+//                score++;
+//
+//                if(!answer3.equals("true")){
+//                    Methods.exitProgram();
+//                    Methods.print("Incorrect");
+//                }
+//                else{
+//                    Methods.print("Question 3\n Is Emilbek agay crush?\n Type 'true' or 'false'");
+//                    String answer4 = system.nextLine();
+//                    score++;
+//
+//                    if(answer4.equals("true")){
+//                        Methods.print("gay");
+//                        Methods.print(String.valueOf(score));
+//                    }
+//                    else{
+//                        Methods.print("shakal");
+//                    }
+//                }
+//            }
+//        }
     }
 }
