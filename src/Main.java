ceb765819f2c43314a030ec08265ed782ae8641a
ahackkk <aktandoolatkeldiev@gmail.com>
2024-10-11 23:45:18 +0600

    TotalUpdate

--- src/Main.java
+++ src/Main.java
@@ -1,152 +1,58 @@
 import java.util.Scanner;
 
-public class Main{
+public class Main {
     public static void main(String[] args) {
 
         long startTime = System.currentTimeMillis();
 
         Scanner system = new Scanner(System.in);
         Methods.print("If you are ready to play, type 'yes'.");
-        String answer = system.nextLine();
+        String answer = system.nextLine().toLowerCase();
 
-        while(!answer.equals("yes")){
+        while (!answer.equals("yes")) {
             Methods.print("When ready, type 'yes'");
-            answer = system.nextLine();
+            answer = system.nextLine().toLowerCase();
         }
+
+        String[][] questions = {
+                {"We can use 100% of our brain at one time", "false", "It's a myth, some parts are used for different purposes"},
+                {"550 / 2 = 225", "false", "Bro, really?"},
+                {"Bananas grow on trees.", "false", "Бананы растут на травянистых растениях, а не на деревьях."},
+                {"The Great Wall of China is visible from space.", "false", "Великая китайская стена не видна из космоса."},
+                {"Goldfish have a memory span of only three seconds.", "false", "Это миф. У золотых рыбок память может длиться несколько месяцев."},
+                {"The human body contains more bacterial cells than human cells.", "true", "В теле человека больше бактериальных клеток, чем человеческих."},
+                {"Venus is the hottest planet in the Solar System.", "true", "Венера действительно самая горячая планета."},
+                {"Hairs of the human continue to grow even after death.", "false", "Волосы не растут после смерти, это миф."},
+                {"People have never seen dinosaurs.", "true", "Динозавры вымерли задолго до появления людей."},
+                {"Finland has land borders with Denmark.", "false", "Между Финляндией и Данией находится вода, Балтийское море."}
+        };
 
         int score = 0;
         int lives = 3;
 
-        Methods.print("\nQuestion 1\n We can use 100% of our brain at one time\n Type 'true' or 'false'");
-        String answer2 = system.nextLine();
-
-        if(answer2.equals("false")){
-            Methods.print("Correct");
-            score++;
+        for (String[] question : questions) {
+            if (askQuestion(system, question[0], question[1], question[2])) {
+                score++;
+            } else {
+                lives--;
+                Methods.checkLives(lives, score);
+            }
         }
-        else {
-            Methods.print("Incorrect\n It's myth, some parts are used for different purposes");
-            lives--;
-        }
-
-        Methods.print("\nQuestion 2\n 550 / 2 = 225\n Type 'true' or 'false'");
-        String answer3 = system.nextLine();
-
-        if(answer3.equals("false")){
-            Methods.print("Correct");
-            score++;
-        }
-        else {
-                Methods.print("Incorrect\n Bro, really?");
-            lives--;
-        }
-
-        Methods.print("\nQuestion 3\n Bananas grow on trees.\n Type 'true' or 'false'");
-        String answer4 = system.nextLine();
-
-        if(answer4.equals("false")){
-            Methods.print("Correct");
-            score++;
-        } else{
-            Methods.print("Incorrect\n Бананы растут не на деревьях, а на травянистых растениях, которые ошибочно принимают за деревья.");
-            lives--;
-        }
-
-        Methods.checkLives(lives, score);
-
-        Methods.print("\nQuestion 4\n The Great Wall of China is visible from space.\n Type 'true' or 'false'");
-        String answer5 = system.nextLine();
-
-        if(answer5.equals("false")){
-            Methods.print("Correct");
-            score++;
-        } else{
-            Methods.print("Incorrect\n Это миф. Великая китайская стена не видна из космоса невооруженным глазом.");
-            lives--;
-        }
-
-        Methods.checkLives(lives, score);
-
-        Methods.print("\nQuestion 5\n Goldfish have a memory span of only three seconds.\n Type 'true' or 'false'");
-        String answer6 = system.nextLine();
-
-        if(answer6.equals("false")){
-            Methods.print("Correct");
-            score++;
-        } else{
-            Methods.print("Incorrect\n Это миф. У золотых рыбок память может длиться несколько месяцев.");
-            lives--;
-        }
-
-        Methods.checkLives(lives, score);
-
-        Methods.print("\nQuestion 6\n The human body contains more bacterial cells than human cells.\n Type 'true' or 'false'");
-        String answer7 = system.nextLine();
-
-        if(answer7.equals("true")){
-            Methods.print("Correct");
-            score++;
-        } else{
-            Methods.print("Incorrect\n В теле человека действительно больше бактериальных клеток, чем человеческих, но они маленькие и составляют малую часть массы.");
-            lives--;
-        }
-
-        Methods.checkLives(lives, score);
-
-        Methods.print("\nQuestion 7\n Venus is the hottest planet in the Solar System\n Type 'true' or 'false'");
-        String answer8 = system.nextLine();
-
-        if(answer8.equals("true")){
-            Methods.print("Correct");
-            score++;
-        } else{
-            Methods.print("Incorrect\n Венера действительно самая горячая планета, несмотря на то, что Меркурий ближе к Солнцу.");
-            lives--;
-        }
-
-        Methods.checkLives(lives, score);
-
-        Methods.print("\nQuestion 8\n Hairs of the human continue to grow even after death\n Type 'true' or 'false'");
-        String answer9 = system.nextLine();
-
-        if(answer9.equals("false")){
-            Methods.print("Correct");
-            score++;
-        } else{
-            Methods.print("Incorrect\n Это миф; волосы и ногти не растут после смерти.");
-            lives--;
-        }
-
-        Methods.checkLives(lives, score);
-
-        Methods.print("\nQuestion 9\n People have never seen dinosaurs\n Type 'true' or 'false'");
-        String answer10 = system.nextLine();
-
-        if(answer10.equals("true")){
-            Methods.print("Correct");
-            score++;
-        } else{
-            Methods.print("Incorrect\n Первые динозавры появились около 230 миллионов лет назад, а первые люди - лишь 200 тысяч лет назад");
-            lives--;
-        }
-
-        Methods.checkLives(lives, score);
-
-        Methods.print("\nQuestion 10\n Finland have land borders with Denmark\n Type 'true' or 'false'");
-        String answer11 = system.nextLine();
-
-        if(answer11.equals("false")){
-            Methods.print("Correct");
-            score++;
-        } else{
-            Methods.print("Incorrect\n Между Финляндией и Данией находится вода, а именно Балтийское море и пролив Эресунн");
-            lives--;
-        }
-
-        Methods.checkLives(lives, score);
 
         long endTime = System.currentTimeMillis();
         long totalTime = (endTime - startTime) / 1000; // время в секундах
-        Methods.print("Your score is " + score + " and you took " + totalTime + " seconds " + "with " + lives + " leftno");
+        Methods.print("Your score is " + score + " and you took " + totalTime + " seconds with " + lives + " lives left.");
+    }
+
+    public static boolean askQuestion(Scanner system, String question, String correctAnswer, String explanation) {
+        Methods.print("\n" + question + "\nType 'true' or 'false'");
+        String userAnswer = system.nextLine().toLowerCase();
+        if (userAnswer.equals(correctAnswer)) {
+            Methods.print("Correct");
+            return true;
+        } else {
+            Methods.print("Incorrect\n" + explanation);
+            return false;
+        }
     }
 }
